60a06e787a113d8b27b3ade3623ce6e330db306e
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-02-18 12:07:42 -0300

    Atualizando sass biblioteca e componente form para tela de login.

--- Bliblioteca 2020/js/login-form.js
+++ Bliblioteca 2020/js/login-form.js
@@ -1,5 +1,5 @@
 $(function() {
-	$(".btn").click(function() {
+	$(".btn-entrar , .btn-cadastra-se").click(function() {
 		$(".form-signin").toggleClass("form-signin-left");
     $(".form-signup").toggleClass("form-signup-left");
     $(".frame").toggleClass("frame-long");
@@ -7,26 +7,5 @@ $(function() {
     $(".signin-active").toggleClass("signin-inactive");
     $(".forgot").toggleClass("forgot-left");
     $(this).removeClass("idle").addClass("active");
-	});
-});
-
-$(function() {
-	$(".btn-signup").click(function() {
-  $(".nav").toggleClass("nav-up");
-  $(".form-signup-left").toggleClass("form-signup-down");
-  $(".success").toggleClass("success-left");
-  $(".frame").toggleClass("frame-short");
-	});
-});
-
-$(function() {
-	$(".btn-signin").click(function() {
-  $(".btn-animate").toggleClass("btn-animate-grow");
-  $(".welcome").toggleClass("welcome-left");
-  $(".cover-photo").toggleClass("cover-photo-down");
-  $(".frame").toggleClass("frame-short");
-  $(".profile-photo").toggleClass("profile-photo-down");
-  $(".btn-goback").toggleClass("btn-goback-up");
-  $(".forgot").toggleClass("forgot-fade");
 	});
 });
